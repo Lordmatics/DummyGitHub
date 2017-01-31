4b753504c6501419553448344d229b2606540b69
GachiPC\Ed <edward.hawksworth@outlook.com>
2017-01-31 22:58:36 +0000

    testing123

---
+++ Source/DummyGitHub/MyWorldSettings.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DummyGitHub.h"
+#include "MyWorldSettings.h"
+
+
+
+
---
+++ Source/DummyGitHub/MyWorldSettings.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/WorldSettings.h"
+#include "MyWorldSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUMMYGITHUB_API AMyWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+	//hehexd
+	
+	
+};
